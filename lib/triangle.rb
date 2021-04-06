4cc1f6ae2557f8367d642de7530079d743a30c7d
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-06 16:54:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a,b,c)
+    @a=a
+    @b=b
+    @c=c
+  end
+  
+  def kind
+    if a<=0 || b<=0 || c<=0 || (a+b)<c
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif a==b&&b==c&&a==c
+      :equilateral
+    elsif a==b||b==c||a==c
+      :isosceles
+    elsif a!=b&&b!=c&&a!=c
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "Sides a + b must be greater than side c, and none of your sides can equal 0."
+    end
+  end
+      
 end
